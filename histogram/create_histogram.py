ba1d9bf431a818ca1d59c159dc9e9c650c5a53af
Unknown <wfaengrit@gmail.com>
2018-09-19 14:33:47 +0700

    update own grayscale histogram creation function

--- histogram/create_histogram.py
+++ histogram/create_histogram.py
@@ -1,7 +1,17 @@
 import cv2
 from PIL import Image
 from matplotlib import pyplot as plt
+import numpy as np
 
+def gray_histogram(img):
+    width, height = img.shape
+    H = [0]*256
+    for y in range (0,height):
+        for x in range (0,width):
+            intensity = img[x,y]
+            i = intensity
+            H[i] = H[i] +1
+    return H
 
 file = ['jpg','gif','png','bmp','tif']
 name = 'image'
@@ -12,12 +22,13 @@ for img_type in file:
     img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     split_img = cv2.split(img)
-    o_hist = cv2.calcHist(gray_img,[1],None,[255],[0,255])
+    #o_hist = cv2.calcHist(gray_img,[1],None,[255],[0,255])
+    o_hist2 = gray_histogram(gray_img)
     b_hist = cv2.calcHist(split_img,[0],None,[255],[0,255])
     g_hist = cv2.calcHist(split_img,[1],None,[255],[0,255])
     r_hist = cv2.calcHist(split_img,[2],None,[255],[0,255])
     plt.subplot(6,2,i)
-    plt.plot(o_hist)
+    plt.plot(o_hist2)
     plt.text(10,50,'gray'+img_type)
     i = i+1
     plt.subplot(6,2,i)
